568283f8767ce81d7c4188056814ee31462da3cb
Sai Krishna Bathina <saikrishna.bathina999@gmail.com>
2025-06-12 13:21:44 +0530

    updated port number with env port

--- app.js
+++ app.js
@@ -71,6 +71,8 @@ app.post("/edit/:id", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("🚀 Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
